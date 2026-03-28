f421772fb6eaeee7ec2801887dfa23aaf824a00a
Josh <jawshs08@gmail.com>
2026-03-27 22:54:06 -0400

    New translations analysispage.tsx (Acholi)

---
+++ ach/folder/AnalysisPage.tsx
@@ -0,0 +1,313 @@
+crwdns8959:0crwdne8959:0
+crwdns8961:0crwdne8961:0
+crwdns8963:0crwdne8963:0
+
+crwdns8965:0crwdne8965:0
+  crwdns8967:0crwdne8967:0
+  crwdns8969:0crwdne8969:0
+  crwdns8971:0crwdne8971:0
+  
+  crwdns8973:0crwdne8973:0
+  crwdns8975:0crwdne8975:0
+  crwdns8977:0crwdne8977:0
+  crwdns8979:0crwdne8979:0
+  crwdns8981:0crwdne8981:0
+  crwdns8983:0crwdne8983:0
+
+  crwdns8985:0crwdne8985:0
+    crwdns8987:0crwdne8987:0
+  crwdns8989:0crwdne8989:0
+
+  crwdns8991:0crwdne8991:0
+  crwdns8993:0crwdne8993:0
+    crwdns8995:0crwdne8995:0
+      crwdns8997:0crwdne8997:0
+      crwdns8999:0crwdne8999:0
+      crwdns9001:0crwdne9001:0
+    crwdns9003:0crwdne9003:0
+  crwdns9005:0crwdne9005:0
+
+  crwdns9007:0crwdne9007:0
+    crwdns9009:0crwdne9009:0
+    crwdns9011:0crwdne9011:0
+    
+    crwdns9013:0crwdne9013:0
+      crwdns9015:0crwdne9015:0
+      crwdns9017:0crwdne9017:0
+      crwdns9019:0crwdne9019:0
+      
+      crwdns9021:0crwdne9021:0
+        crwdns9023:0${response.status}crwdnd9023:0${response.statusText}crwdne9023:0
+      crwdns9025:0crwdne9025:0
+      
+      crwdns9027:0crwdne9027:0
+      crwdns9029:0crwdne9029:0
+    crwdns9031:0crwdne9031:0
+      crwdns9033:0crwdne9033:0
+      crwdns9035:0crwdne9035:0
+    crwdns9037:0crwdne9037:0
+      crwdns9039:0crwdne9039:0
+    crwdns9041:0crwdne9041:0
+  crwdns9043:0crwdne9043:0
+
+  crwdns9045:0crwdne9045:0
+    crwdns9047:0crwdne9047:0
+    crwdns9049:0crwdne9049:0 crwdns9051:0crwdne9051:0
+    crwdns9053:0crwdne9053:0
+    crwdns9055:0crwdne9055:0
+    
+    crwdns9057:0crwdne9057:0
+    crwdns9059:0crwdne9059:0 crwdns9061:0crwdne9061:0
+    crwdns9063:0crwdne9063:0 crwdns9065:0crwdne9065:0
+    crwdns9067:0crwdne9067:0 crwdns9069:0crwdne9069:0
+  crwdns9071:0crwdne9071:0
+
+  crwdns9073:0crwdne9073:0
+    crwdns9075:0crwdne9075:0
+    
+    crwdns9077:0crwdne9077:0
+      crwdns9079:0crwdne9079:0
+    crwdns9081:0crwdne9081:0
+    
+    crwdns9083:0crwdne9083:0
+    
+    crwdns9085:0crwdne9085:0
+      crwdns9087:0crwdne9087:0
+        crwdns9089:0crwdne9089:0
+        crwdns9091:0crwdne9091:0
+          crwdns9093:0crwdne9093:0
+        crwdns9095:0crwdne9095:0
+        crwdns9097:0crwdne9097:0
+          crwdns9099:0crwdne9099:0 
+            crwdns9101:0crwdne9101:0 crwdns9103:0crwdne9103:0 
+            crwdns9105:0crwdne9105:0
+        crwdns9107:0crwdne9107:0
+      crwdns9109:0crwdne9109:0
+      
+      crwdns9111:0crwdne9111:0
+        crwdns9113:0crwdne9113:0
+      crwdns9115:0crwdne9115:0
+      
+      crwdns9117:0crwdne9117:0
+      crwdns9119:0crwdne9119:0 
+        crwdns9121:0crwdne9121:0 crwdns9123:0crwdne9123:0
+        crwdns9125:0crwdne9125:0
+      crwdns9127:0crwdne9127:0
+    crwdns9129:0crwdne9129:0
+      crwdns9131:0crwdne9131:0
+      crwdns9133:0crwdne9133:0 
+        crwdns9135:0crwdne9135:0 crwdns9137:0crwdne9137:0
+        crwdns9139:0crwdne9139:0
+      crwdns9141:0crwdne9141:0
+    crwdns9143:0crwdne9143:0
+      crwdns9145:0crwdne9145:0
+      crwdns9147:0crwdne9147:0
+    crwdns9149:0crwdne9149:0
+  crwdns9151:0crwdne9151:0
+
+  crwdns9153:0crwdne9153:0
+    crwdns9155:0crwdne9155:0
+    crwdns9157:0crwdne9157:0
+  crwdns9159:0crwdne9159:0
+
+  crwdns9161:0crwdne9161:0
+    crwdns9163:0crwdne9163:0
+      crwdns9165:0crwdne9165:0
+      crwdns9167:0crwdne9167:0
+        crwdns9169:0crwdne9169:0
+      crwdns9171:0crwdne9171:0
+      
+      crwdns9173:0crwdne9173:0
+        crwdns9175:0crwdne9175:0
+        crwdns9177:0crwdne9177:0
+          crwdns9179:0crwdne9179:0
+            crwdns9181:0crwdne9181:0
+              crwdns9183:0crwdne9183:0
+            crwdns9185:0crwdne9185:0
+          crwdns9187:0crwdne9187:0
+          
+          crwdns9189:0crwdne9189:0
+              crwdns9191:0crwdne9191:0 
+                crwdns9193:0{fetchAnalysisData}crwdne9193:0
+                crwdns9195:0crwdne9195:0
+              crwdns9197:0crwdne9197:0
+                crwdns9199:0crwdne9199:0
+              crwdns9201:0crwdne9201:0
+          crwdns9203:0crwdne9203:0
+          
+          crwdns9205:0crwdne9205:0
+            crwdns9207:0crwdne9207:0
+              crwdns9209:0crwdne9209:0
+                crwdns9211:0crwdne9211:0
+                
+                crwdns9213:0crwdne9213:0
+                  crwdns9215:0crwdne9215:0 
+                    crwdns9217:0${analysisData.plot_url}crwdne9217:0
+                    crwdns9219:0crwdne9219:0
+                    crwdns9221:0crwdne9221:0
+                  crwdns9223:0crwdne9223:0
+                crwdns9225:0crwdne9225:0
+                
+                crwdns9227:0crwdne9227:0
+                  crwdns9229:0crwdne9229:0
+                    crwdns9231:0crwdne9231:0
+                    crwdns9233:0crwdne9233:0
+                      crwdns9235:0crwdne9235:0
+                      crwdns9237:0crwdne9237:0 crwdns9239:0crwdne9239:0
+                        crwdns9241:0crwdne9241:0
+                      crwdns9243:0crwdne9243:0
+                        crwdns9245:0crwdne9245:0
+                      crwdns9247:0crwdne9247:0
+                    crwdns9249:0crwdne9249:0
+                    crwdns9251:0crwdne9251:0
+                      crwdns9253:0crwdne9253:0
+                    crwdns9255:0crwdne9255:0
+                  crwdns9257:0crwdne9257:0
+                  
+                  crwdns9259:0crwdne9259:0
+                    crwdns9261:0crwdne9261:0
+                    crwdns9263:0crwdne9263:0
+                      crwdns9265:0{analysisData.stats.data_count}crwdne9265:0
+                    crwdns9267:0crwdne9267:0
+                    crwdns9269:0crwdne9269:0
+                      crwdns9271:0crwdne9271:0
+                        crwdns9273:0crwdne9273:0
+                      crwdns9275:0crwdne9275:0
+                    crwdns9277:0crwdne9277:0
+                  crwdns9279:0crwdne9279:0
+                crwdns9281:0crwdne9281:0
+                
+                crwdns9283:0crwdne9283:0
+                  crwdns9285:0crwdne9285:0
+                  crwdns9287:0crwdne9287:0
+                    crwdns9289:0crwdne9289:0 crwdns9291:0crwdne9291:0
+                      crwdns9293:0crwdne9293:0 crwdns9295:0crwdne9295:0
+                    crwdns9297:0crwdne9297:0 crwdns9299:0crwdne9299:0
+                      crwdns9301:0crwdne9301:0
+                    crwdns9303:0crwdne9303:0
+                      crwdns9305:0crwdne9305:0 crwdns9307:0crwdne9307:0 crwdns9309:0crwdne9309:0
+                    crwdns9311:0crwdne9311:0
+                  crwdns9313:0crwdne9313:0
+                  crwdns9315:0crwdne9315:0 crwdns9317:0crwdne9317:0
+                    crwdns9319:0crwdne9319:0
+                  crwdns9321:0crwdne9321:0
+                    crwdns9323:0crwdne9323:0 crwdns9325:0crwdne9325:0
+                  crwdns9327:0crwdne9327:0
+              crwdns9329:0crwdne9329:0
+            crwdns9331:0crwdne9331:0
+            
+              crwdns9333:0crwdne9333:0
+                crwdns9335:0crwdne9335:0 
+                  crwdns9337:0crwdne9337:0
+                  crwdns9339:0crwdne9339:0
+                crwdns9341:0crwdne9341:0
+                  crwdns9343:0crwdne9343:0
+                crwdns9345:0crwdne9345:0
+                crwdns9347:0crwdne9347:0 
+                  crwdns9349:0crwdne9349:0
+                  crwdns9351:0crwdne9351:0
+                crwdns9353:0crwdne9353:0
+                  crwdns9355:0crwdne9355:0
+                crwdns9357:0crwdne9357:0
+              crwdns9359:0crwdne9359:0
+            crwdns9361:0crwdne9361:0
+          crwdns9363:0crwdne9363:0
+        crwdns9365:0crwdne9365:0
+
+        crwdns9367:0crwdne9367:0
+        crwdns9369:0crwdne9369:0
+          crwdns9371:0crwdne9371:0
+            crwdns9373:0crwdne9373:0
+              crwdns9375:0crwdne9375:0
+                crwdns9377:0crwdne9377:0
+                crwdns9379:0crwdne9379:0
+              crwdns9381:0crwdne9381:0
+              crwdns9383:0crwdne9383:0 
+                crwdns9385:0crwdne9385:0
+                crwdns9387:0crwdne9387:0
+              crwdns9389:0crwdne9389:0
+                crwdns9391:0crwdne9391:0
+                  crwdns9393:0{2}crwdne9393:0
+                crwdns9395:0crwdne9395:0
+              crwdns9397:0crwdne9397:0
+            crwdns9399:0crwdne9399:0
+            
+            crwdns9401:0crwdne9401:0
+              crwdns9403:0crwdne9403:0 crwdns9405:0crwdne9405:0
+                crwdns9407:0crwdne9407:0
+                  crwdns9409:0crwdne9409:0 
+                    crwdns9411:0crwdne9411:0 
+                    crwdns9413:0crwdne9413:0
+                crwdns9415:0crwdne9415:0
+              crwdns9417:0crwdne9417:0
+                crwdns9419:0crwdne9419:0
+                  crwdns9421:0crwdne9421:0
+                    crwdns9423:0crwdne9423:0 
+                      crwdns9425:0{index}crwdne9425:0 
+                      crwdns9427:0crwdne9427:0 crwdns9429:0crwdne9429:0
+                    crwdns9431:0crwdne9431:0
+                      crwdns9433:0crwdne9433:0 
+                        crwdns9435:0crwdne9435:0
+                          crwdns9437:0crwdne9437:0 
+                            crwdns9439:0crwdne9439:0 crwdns9441:0crwdne9441:0 
+                            crwdns9443:0crwdne9443:0
+                        crwdns9445:0crwdne9445:0
+                      crwdns9447:0crwdne9447:0
+                        crwdns9449:0{msg.content}crwdne9449:0
+                      crwdns9451:0crwdne9451:0
+                    crwdns9453:0crwdne9453:0
+                  crwdns9455:0crwdne9455:0
+                  crwdns9457:0crwdne9457:0
+                    crwdns9459:0crwdne9459:0
+                      crwdns9461:0crwdne9461:0
+                        crwdns9463:0crwdne9463:0
+                          crwdns9465:0crwdne9465:0
+                          crwdns9467:0{{animationDelay: '0.2s'}}crwdne9467:0
+                          crwdns9469:0{{animationDelay: '0.4s'}}crwdne9469:0
+                        crwdns9471:0crwdne9471:0
+                      crwdns9473:0crwdne9473:0
+                    crwdns9475:0crwdne9475:0
+                  crwdns9477:0crwdne9477:0
+                crwdns9479:0crwdne9479:0
+              crwdns9481:0crwdne9481:0
+            crwdns9483:0crwdne9483:0
+            
+            crwdns9485:0{handleSubmit}crwdne9485:0
+              crwdns9487:0crwdne9487:0
+                crwdns9489:0crwdne9489:0
+                  crwdns9491:0crwdne9491:0
+                  crwdns9493:0{newMessage}crwdne9493:0
+                  crwdns9495:0crwdne9495:0
+                  crwdns9497:0crwdne9497:0
+                  crwdns9499:0crwdne9499:0
+                  crwdns9501:0crwdne9501:0
+                crwdns9503:0crwdne9503:0
+                crwdns9505:0crwdne9505:0
+                  crwdns9507:0crwdne9507:0
+                  crwdns9509:0crwdne9509:0
+                  crwdns9511:0crwdne9511:0
+                crwdns9513:0crwdne9513:0
+                  crwdns9515:0crwdne9515:0
+                    crwdns9517:0crwdne9517:0
+                  crwdns9519:0crwdne9519:0
+                crwdns9521:0crwdne9521:0
+              crwdns9523:0crwdne9523:0
+            crwdns9525:0crwdne9525:0
+          crwdns9527:0crwdne9527:0
+        crwdns9529:0crwdne9529:0
+
+        crwdns9531:0crwdne9531:0
+        crwdns9533:0crwdne9533:0
+          crwdns9535:0crwdne9535:0
+            crwdns9537:0crwdne9537:0
+            crwdns9539:0crwdne9539:0
+          crwdns9541:0crwdne9541:0
+            crwdns9543:0crwdne9543:0
+              crwdns9545:0{2}crwdne9545:0
+            crwdns9547:0crwdne9547:0
+          crwdns9549:0crwdne9549:0
+        crwdns9551:0crwdne9551:0
+      crwdns9553:0crwdne9553:0
+    crwdns9555:0crwdne9555:0
+  crwdns9557:0crwdne9557:0
+crwdns9559:0crwdne9559:0
