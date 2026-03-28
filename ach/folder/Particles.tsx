af3808c669ecf97f78f987555e5c91d45f95d7bf
Josh <jawshs08@gmail.com>
2026-03-27 22:54:02 -0400

    New translations particles.tsx (Acholi)

---
+++ ach/folder/Particles.tsx
@@ -0,0 +1,98 @@
+crwdns8269:0{ initParticlesEngine }crwdne8269:0
+crwdns8271:0crwdne8271:0
+crwdns8273:0{ loadSlim }crwdne8273:0
+
+crwdns8275:0crwdne8275:0
+  crwdns8277:0crwdne8277:0
+
+  crwdns8279:0crwdne8279:0
+    crwdns8281:0crwdne8281:0
+      crwdns8283:0crwdne8283:0
+    crwdns8285:0crwdne8285:0
+      crwdns8287:0crwdne8287:0
+    crwdns8289:0crwdne8289:0
+  crwdns8291:0crwdne8291:0
+
+  crwdns8293:0crwdne8293:0
+    crwdns8295:0crwdne8295:0
+  crwdns8297:0crwdne8297:0
+
+  crwdns8299:0crwdne8299:0
+    crwdns8301:0crwdne8301:0
+      crwdns8303:0crwdne8303:0
+        crwdns8305:0crwdne8305:0
+          crwdns8307:0crwdne8307:0
+        crwdns8309:0crwdne8309:0
+      crwdns8311:0crwdne8311:0
+      crwdns8313:0crwdne8313:0
+      crwdns8315:0crwdne8315:0
+        crwdns8317:0crwdne8317:0
+          crwdns8319:0crwdne8319:0
+            crwdns8321:0crwdne8321:0
+            crwdns8323:0crwdne8323:0
+          crwdns8325:0crwdne8325:0
+          crwdns8327:0crwdne8327:0
+            crwdns8329:0crwdne8329:0
+            crwdns8331:0crwdne8331:0
+          crwdns8333:0crwdne8333:0
+        crwdns8335:0crwdne8335:0
+        crwdns8337:0crwdne8337:0
+          crwdns8339:0crwdne8339:0
+            crwdns8341:0crwdne8341:0
+            crwdns8343:0crwdne8343:0
+          crwdns8345:0crwdne8345:0
+          crwdns8347:0crwdne8347:0
+            crwdns8349:0crwdne8349:0
+          crwdns8351:0crwdne8351:0
+        crwdns8353:0crwdne8353:0
+      crwdns8355:0crwdne8355:0
+      crwdns8357:0crwdne8357:0
+        crwdns8359:0crwdne8359:0
+          crwdns8361:0crwdne8361:0
+        crwdns8363:0crwdne8363:0
+        crwdns8365:0crwdne8365:0
+          crwdns8367:0crwdne8367:0
+          crwdns8369:0crwdne8369:0
+          crwdns8371:0crwdne8371:0
+          crwdns8373:0crwdne8373:0
+          crwdns8375:0crwdne8375:0
+        crwdns8377:0crwdne8377:0
+        crwdns8379:0crwdne8379:0
+          crwdns8381:0crwdne8381:0
+          crwdns8383:0crwdne8383:0
+          crwdns8385:0crwdne8385:0
+            crwdns8387:0crwdne8387:0
+          crwdns8389:0crwdne8389:0
+          crwdns8391:0crwdne8391:0
+          crwdns8393:0crwdne8393:0
+          crwdns8395:0crwdne8395:0
+        crwdns8397:0crwdne8397:0
+        crwdns8399:0crwdne8399:0
+          crwdns8401:0crwdne8401:0
+            crwdns8403:0crwdne8403:0
+          crwdns8405:0crwdne8405:0
+          crwdns8407:0crwdne8407:0
+        crwdns8409:0crwdne8409:0
+        crwdns8411:0crwdne8411:0
+          crwdns8413:0crwdne8413:0
+        crwdns8415:0crwdne8415:0
+        crwdns8417:0crwdne8417:0
+          crwdns8419:0crwdne8419:0
+        crwdns8421:0crwdne8421:0
+        crwdns8423:0crwdne8423:0
+          crwdns8425:0crwdne8425:0
+        crwdns8427:0crwdne8427:0
+      crwdns8429:0crwdne8429:0
+      crwdns8431:0crwdne8431:0
+    crwdns8433:0crwdne8433:0
+    crwdns8435:0crwdne8435:0
+  crwdns8437:0crwdne8437:0
+
+  crwdns8439:0crwdne8439:0
+    crwdns8441:0{props.id}crwdnd8441:0{particlesLoaded}crwdnd8441:0{options}crwdne8441:0
+  crwdns8443:0crwdne8443:0
+
+  crwdns8445:0crwdne8445:0
+crwdns8447:0crwdne8447:0
+
+crwdns8449:0crwdne8449:0
