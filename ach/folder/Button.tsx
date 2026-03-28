d7e0a4b1afa9a7ac5b77d223208cd0d748cd6d0b
Josh <jawshs08@gmail.com>
2026-03-27 22:54:00 -0400

    New translations button.tsx (Acholi)

---
+++ ach/folder/Button.tsx
@@ -0,0 +1,9 @@
+crwdns8187:0{ text }crwdne8187:0
+  crwdns8189:0crwdne8189:0
+    crwdns8191:0crwdne8191:0
+      crwdns8193:0{text}crwdne8193:0
+    crwdns8195:0crwdne8195:0
+  crwdns8197:0crwdne8197:0
+crwdns8199:0crwdne8199:0
+
+crwdns8201:0crwdne8201:0
