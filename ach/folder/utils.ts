d6331b436118d4b85523dcb3ac5b34e9abe65500
Josh <jawshs08@gmail.com>
2026-03-27 22:54:03 -0400

    New translations utils.ts (Acholi)

---
+++ ach/folder/utils.ts
@@ -0,0 +1,5 @@
+import { clsx, type ClassValue } from "crwdns8877:0crwdne8877:0";
+import { twMerge } from "crwdns8879:0crwdne8879:0";
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
