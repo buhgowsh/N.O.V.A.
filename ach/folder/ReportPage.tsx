f19fab33adc5cbdff3db241ff26f0c2db7dc118d
Josh <jawshs08@gmail.com>
2026-03-27 22:54:09 -0400

    New translations reportpage.tsx (Acholi)

---
+++ ach/folder/ReportPage.tsx
@@ -0,0 +1,68 @@
+crwdns10259:0crwdne10259:0
+
+crwdns10261:0crwdne10261:0
+
+    crwdns10263:0crwdne10263:0
+
+    crwdns10265:0crwdne10265:0
+        crwdns10267:0crwdne10267:0
+
+        crwdns10269:0crwdne10269:0
+        crwdns10271:0crwdne10271:0
+        crwdns10273:0{apiKey}crwdne10273:0
+        crwdns10275:0crwdne10275:0
+
+            crwdns10277:0crwdne10277:0
+            crwdns10279:0crwdne10279:0
+            crwdns10281:0crwdne10281:0
+                crwdns10283:0crwdne10283:0
+            crwdns10285:0crwdne10285:0
+            crwdns10287:0crwdne10287:0
+                crwdns10289:0crwdne10289:0
+                    crwdns10291:0crwdne10291:0
+                    crwdns10293:0crwdne10293:0
+                        crwdns10295:0crwdne10295:0
+                    crwdns10297:0crwdne10297:0
+
+                    crwdns10299:0crwdne10299:0
+                    crwdns10301:0crwdne10301:0
+
+                    crwdns10303:0crwdne10303:0
+                    crwdns10305:0crwdne10305:0
+
+                    crwdns10307:0crwdne10307:0
+
+                    crwdns10309:0crwdne10309:0
+                    crwdns10311:0crwdne10311:0
+                        crwdns10313:0crwdne10313:0
+                        crwdns10315:0crwdne10315:0
+                            crwdns10317:0crwdne10317:0
+                            crwdns10319:0crwdne10319:0
+                            crwdns10321:0crwdne10321:0
+                        crwdns10323:0crwdne10323:0
+                        crwdns10325:0crwdne10325:0
+                        crwdns10327:0crwdne10327:0
+                            crwdns10329:0crwdne10329:0
+                            crwdns10331:0crwdne10331:0
+                            crwdns10333:0crwdne10333:0
+                        crwdns10335:0crwdne10335:0
+                        crwdns10337:0crwdne10337:0
+                        crwdns10339:0crwdne10339:0
+                            crwdns10341:0crwdne10341:0
+                            crwdns10343:0crwdne10343:0
+                            crwdns10345:0crwdne10345:0
+                        crwdns10347:0crwdne10347:0
+                        crwdns10349:0crwdne10349:0
+                        crwdns10351:0crwdne10351:0
+                            crwdns10353:0crwdne10353:0
+                            crwdns10355:0crwdne10355:0
+                            crwdns10357:0crwdne10357:0
+                        crwdns10359:0crwdne10359:0
+                    crwdns10361:0crwdne10361:0
+                crwdns10363:0crwdne10363:0
+            crwdns10365:0crwdne10365:0
+        crwdns10367:0crwdne10367:0
+    crwdns10369:0crwdne10369:0
+crwdns10371:0crwdne10371:0
+
+crwdns10373:0crwdne10373:0
